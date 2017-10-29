144f60ba5f2cbe5309a64df41f7b165884749002
janus1001 <janusz.cimortowicz@gmail.com>
2017-10-29 16:10:44 +0100

    Wall Building
    Działa budowanie pojedynczych ścian

--- Assets/Scripts/BuildingLineCreator.cs
+++ Assets/Scripts/BuildingLineCreator.cs
@@ -9,28 +9,36 @@ public class BuildingLineCreator : MonoBehaviour {
     public GameObject punktD;
     public GameObject punktStart; //A
     public GameObject punktEnd; //B
+    [Tooltip("Szerokość ściany")]
     public float l = 0.1f; //Grubość ściany
+    WallMeshCreator meshBuilder;
 
-
-    
-
-
+    void Start ()
+    {
+        meshBuilder = GetComponent<WallMeshCreator>();
+    }
     void Update () {
         if (Input.GetMouseButton(0))
         {
             RaycastHit hitRay;
             if (Physics.Raycast(Camera.main.ScreenPointToRay(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0)), out hitRay, 128))
             {
-                punktEnd.transform.position = hitRay.point;
+                punktEnd.transform.position = Vector3Int.RoundToInt(hitRay.point);
             }
-
+            if (punktStart.transform.position == punktEnd.transform.position)
+                return;
             float sinAlpha = (punktEnd.transform.position.z - punktStart.transform.position.z) / (Mathf.Sqrt(Mathf.Pow((punktEnd.transform.position.x - punktStart.transform.position.x), 2) + Mathf.Pow((punktEnd.transform.position.z - punktStart.transform.position.z), 2)));
             float cosAlpha = (punktEnd.transform.position.x - punktStart.transform.position.x) / (Mathf.Sqrt(Mathf.Pow((punktEnd.transform.position.x - punktStart.transform.position.x), 2) + Mathf.Pow((punktEnd.transform.position.z - punktStart.transform.position.z), 2)));
-
+            
             punktA.transform.position = new Vector3(punktStart.transform.position.x + l * sinAlpha, 0, punktStart.transform.position.z - l * cosAlpha);
             punktB.transform.position = new Vector3(punktStart.transform.position.x - l * sinAlpha, 0, punktStart.transform.position.z + l * cosAlpha);
             punktC.transform.position = new Vector3(punktEnd.transform.position.x + l * sinAlpha, 0, punktEnd.transform.position.z - l * cosAlpha);
             punktD.transform.position = new Vector3(punktEnd.transform.position.x - l * sinAlpha, 0, punktEnd.transform.position.z + l * cosAlpha);
+
+            meshBuilder.BuildMesh(new Vector3(punktStart.transform.position.x + l * sinAlpha, 0, punktStart.transform.position.z - l * cosAlpha),
+                new Vector3(punktStart.transform.position.x - l * sinAlpha, 0, punktStart.transform.position.z + l * cosAlpha),
+                new Vector3(punktEnd.transform.position.x + l * sinAlpha, 0, punktEnd.transform.position.z - l * cosAlpha),
+                new Vector3(punktEnd.transform.position.x - l * sinAlpha, 0, punktEnd.transform.position.z + l * cosAlpha));
         }
         else
         {
@@ -39,7 +47,7 @@ public class BuildingLineCreator : MonoBehaviour {
             punktC.transform.position =
             punktD.transform.position =
             punktStart.transform.position =
-            punktEnd.transform.position = Vector3.zero;
+            punktEnd.transform.position = Vector3.down;
         }
 
 		if(Input.GetMouseButtonDown(0))
@@ -47,7 +55,7 @@ public class BuildingLineCreator : MonoBehaviour {
             RaycastHit hitRay;
             if (Physics.Raycast(Camera.main.ScreenPointToRay(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0)), out hitRay, 128))
             {
-                punktStart.transform.position = hitRay.point;
+                punktStart.transform.position = Vector3Int.RoundToInt(hitRay.point);
             }
         }
 	}
---
+++ Assets/Scripts/WallMeshCreator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallMeshCreator : MonoBehaviour
+{
+    public float floorHeight;
+    public void BuildMesh(Vector3 punktA, Vector3 punktB, Vector3 punktC, Vector3 punktD)
+    {
+        Vector3[] newVertices;
+        Vector2[] newUV = new Vector2[0]; //TODO: Dla ściany wyznaczyć wielkość powierzchni
+        int[] newTriangles;
+
+        Mesh mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        newVertices = new Vector3[] { punktA, punktB, punktD, punktC, RaiseY(punktA, floorHeight), RaiseY(punktB, floorHeight), RaiseY(punktD, floorHeight), RaiseY(punktC, floorHeight) };
+        newTriangles = new int[] { 0, 1, 5, 0, 5, 4, 1, 2, 6, 1, 6, 5, 2, 3, 7, 2, 7, 6, 3, 0, 4, 3, 4, 7 };
+      //newTriangles = new int[] { 0, 1, 5, 0, 5, 4, 1, 3, 7, 1, 7, 5, 3, 2, 6, 3, 6, 7, 2, 0, 4, 2, 4, 6 };
+
+
+        mesh.vertices = newVertices;
+        mesh.uv = newUV;
+        mesh.triangles = newTriangles;
+    }
+    Vector3 RaiseY(Vector3 point, float amount)
+    {
+        return new Vector3(point.x, point.y + amount, point.z);
+    }
+}
+
